bc457d8f9a369dffa53bb004fdbef1af0b5ddf64
Gzim-DArdhishta <gzimdardhishta0gmail.com>
2022-04-11 22:19:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        XIMI
+        XIMI hi
       </header>
     </div>
   );
